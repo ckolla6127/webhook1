19799b468dbff287c563d8986be883078d79cbe7
ckolla6127 <105976042+ckolla6127@users.noreply.github.com>
2022-12-19 20:22:20 -0800

    Update scriptedpl.sh

--- scriptedpl.sh
+++ scriptedpl.sh
@@ -1,5 +1,14 @@
-echo "lets build scripted pl"
-echo "learn hard"
-echo "work hard"
-echo "modfiied"
-echo "done"
+node{
+    stage('git checkout'){
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/ckolla6127/webhook1.git']]])
+    }
+    stage('commands'){
+    sh '''echo "building scripted piepline"
+    echo "this is Build phase"
+    echo "thankl you"'''
+    }
+    stage('shell excute'){
+        sh '''cd /home/ec2-user/
+        mkdir -p ckolla'''
+    }
+}
